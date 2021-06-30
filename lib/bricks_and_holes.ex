4f5d3e0a3be0779c51fd26e127d542747bde9a9f
Kerry Buckley <kerryjbuckley@gmail.com>
2021-06-30 14:09:13 +0100

    New project

---
+++ lib/bricks_and_holes.ex
@@ -0,0 +1,18 @@
+defmodule BricksAndHoles do
+  @moduledoc """
+  Documentation for `BricksAndHoles`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BricksAndHoles.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bricks_and_holes_test.exs
@@ -0,0 +1,8 @@
+defmodule BricksAndHolesTest do
+  use ExUnit.Case
+  doctest BricksAndHoles
+
+  test "greets the world" do
+    assert BricksAndHoles.hello() == :world
+  end
+end
