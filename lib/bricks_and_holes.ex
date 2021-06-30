fa9c5e48632e6c505dd91e9b59ac9981b722bea8
Kerry Buckley <kerryjbuckley@gmail.com>
2021-06-30 14:14:20 +0100

    Remove generated boilerplate

--- lib/bricks_and_holes.ex
+++ lib/bricks_and_holes.ex
@@ -1,18 +1,2 @@
 defmodule BricksAndHoles do
-  @moduledoc """
-  Documentation for `BricksAndHoles`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BricksAndHoles.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/bricks_and_holes_test.exs
+++ test/bricks_and_holes_test.exs
@@ -1,8 +1,4 @@
 defmodule BricksAndHolesTest do
   use ExUnit.Case
   doctest BricksAndHoles
-
-  test "greets the world" do
-    assert BricksAndHoles.hello() == :world
-  end
 end
